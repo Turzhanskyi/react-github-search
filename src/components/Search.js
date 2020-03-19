34952350d957e44ce5593bd98da025c6627a7e46
Turzhanskyi <turzhansky81@gmail.com>
2020-03-19 21:55:28 +0200

    116. Компонент поиска

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AlertContext } from "../context/alert/alertContext";
 
 export const Search = () => {
+  const [value, setValue] = useState("");
   const { show } = useContext(AlertContext);
 
   const onSubmit = event => {
-    if (event.key === "Enter") {
-      show("This is alert!!!");
+    if (event.key !== "Enter") {
+      return;
+    }
+
+    if (value.trim()) {
+      console.log("Make request with ", value);
+    } else {
+      show("Уведіть дані користувача");
     }
   };
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Уведіть нікнейм користувача..."
+        value={value}
+        onChange={event => setValue(event.target.value)}
         onKeyPress={onSubmit}
       />
     </div>
