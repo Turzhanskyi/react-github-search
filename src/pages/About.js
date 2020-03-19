c6e0ce7c70eb2937c01c1971de0a30a160687e7a
Turzhanskyi <turzhansky81@gmail.com>
2020-03-19 11:25:38 +0200

    112. Добавление роутинга

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Інформація</h1>
+        <p className="lead">
+          Версія програми <srtong>1.0.0</srtong>
+        </p>
+      </div>
     </div>
   );
 };
