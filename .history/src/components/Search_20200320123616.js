83113af5ebf098554e7d991bc3c228b0878b590f
Turzhanskyi <turzhansky81@gmail.com>
2020-03-20 12:46:47 +0200

    119. Получение данных с github

---
+++ .history/src/components/Search_20200320123616.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import { AlertContext } from "../context/alert/alertContext";
+
+export const Search = () => {
+  const [value, setValue] = useState("");
+  const { show } = useContext(AlertContext);
+  const github = useContext(G)
+
+  const onSubmit = event => {
+    if (event.key !== "Enter") {
+      return;
+    }
+
+    if (value.trim()) {
+      console.log("Make request with ", value);
+    } else {
+      show("Уведіть дані користувача");
+    }
+  };
+
+  return (
+    <div className="form-group">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Уведіть нікнейм користувача..."
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        onKeyPress={onSubmit}
+      />
+    </div>
+  );
+};
---
+++ .history/src/context/github/GithubState_20200320122943.js
@@ -0,0 +1,75 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import { GithubContext } from "./githubContext";
+import { githubReducer } from "./githubReducer";
+import {
+  SEARCH_USERS,
+  GET_USER,
+  GET_REPOS,
+  CLEAR_USERS,
+  SET_LOADING
+} from "../types";
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+export const GithubState = ({ children }) => {
+  const initialState = {
+    user: {},
+    users: [],
+    loading: false,
+    repos: []
+  };
+
+  const [state, dispath] = useReducer(githubReducer, initialState);
+
+  const search = async value => {
+    setLoading();
+
+    const responc
+    dispath({
+      type: SEARCH_USERS,
+      payload: []
+    });
+  };
+
+  const getUser = async name => {
+    setLoading();
+    dispath({
+      type: GET_USER,
+      payload: []
+    });
+  };
+
+  const getRepos = async name => {
+    setLoading();
+    dispath({
+      type: GET_REPOS,
+      payload: []
+    });
+  };
+
+  const clearUsers = () => dispath({ type: CLEAR_USERS });
+
+  const setLoading = () => dispath({ type: SET_LOADING });
+
+  const { user, users, repos, loading } = state;
+
+  return (
+    <GithubContext.Provider
+      value={{
+        setLoading,
+        search,
+        getUser,
+        getRepos,
+        clearUsers,
+        user,
+        users,
+        repos,
+        loading
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
